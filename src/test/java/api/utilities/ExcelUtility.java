faa6a871b0c1f57f4ffa966810a9173340d7dc46
sohan8484 <shnkhn84@gmail.com>
2023-10-20 09:56:35 +0530

    add.

--- src/test/java/api/utilities/ExcelUtility.java
+++ src/test/java/api/utilities/ExcelUtility.java
@@ -1,5 +1,23 @@
 package api.utilities;
 
-public class ExcelUtility {
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ExcelUtility {
+    public String readExcel(String sheetName,int row, int column) throws IOException {
+        FileInputStream fis = new FileInputStream("paht");
+        Workbook wb = WorkbookFactory.create(fis);
+        return wb.getSheet(sheetName).getRow(row).getCell(column).getStringCellValue();
+    }
+    public void writeExcel(String sheetName, int row, int column, String data) throws IOException {
+        FileInputStream fis = new FileInputStream("pat");
+        Workbook wb = WorkbookFactory.create(fis);
+        wb.createSheet(sheetName).createRow(row).createCell(column).setCellValue(data);
+        FileOutputStream fos = new FileOutputStream("path");
+        wb.write(fos);
+    }
 }
